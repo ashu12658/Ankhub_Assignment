316bbe02d1b88e53390769038e795e5eaab64a28
ashu12658 <ashishghatol098@gmail.com>
2025-08-09 18:37:11 +0530

    Update adminRoutes.js

--- admin-service/router/adminRoutes.js
+++ admin-service/router/adminRoutes.js
@@ -1,9 +1,13 @@
+
+
 const express = require('express');
 const router = express.Router();
-const {register,login,fetchUsersFromService,getAllOrdersFromOrderService} = require('../controller/adminControler');
+const {register,login,fetchUsersFromService,getAllOrdersFromOrderService,getUserByIdFromUserService,deleteUserByIdFromUserService} = require('../controller/adminControler');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/fetch-user',fetchUsersFromService);
-router.get('/get-orders',getAllOrdersFromOrderService)
+router.get('/get-orders',getAllOrdersFromOrderService);
+router.get('user/:id',getUserByIdFromUserService);
+router.delete('/delete/:id',deleteUserByIdFromUserService);
 module.exports = router;
